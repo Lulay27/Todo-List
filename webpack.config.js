d624f8ab3aad82d2d52e7319739c51f7444b72d0
SteezyLeezy <ericle201727@gmail.com>
2022-06-02 11:07:38 -0700

    added assets for loading pictures/ sttyling to be bundled

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,16 @@ module.exports = {
         // assetModuleFilename: '[name][ext]',     // makes names when bundled in dist
         // clean: true,                            // removes excess bundles in dist
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
 };
