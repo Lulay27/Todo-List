8e6bc25936155eb068d440d677c86c023094793b
SteezyLeezy <ericle201727@gmail.com>
2022-06-02 00:13:09 -0700

    added dev mode into config added build code in package.json so i can run with npm run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: '[name][ext]',     // makes names when bundled in dist
+        clean: true,                            // removes excess bundles in dist
+    },
 };
