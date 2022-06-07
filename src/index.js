ede2707a07b3063512a35a7b456d5a515a67eb39
SteezyLeezy <ericle201727@gmail.com>
2022-06-07 01:23:40 -0700

    added more styling. Added side bar transition and two on click events, need to configure code so that clicking on the header will close the side-bar, but have to make code more optimal with if != side-bar close the sidebar somehow

--- src/index.js
+++ src/index.js
@@ -2,9 +2,38 @@ import _ from 'lodash';
 import './styles/style.css';
 // import Icon from './components/menu.png';
 
+const menuBtn = document.querySelector('.menu-btn');
+const nonMenu = document.querySelector('.main');
+const notSide = document.querySelectorAll('body :not(.side-bar');
 
+menuBtn.addEventListener('click',render);
+nonMenu.addEventListener('click',renderOff);
 
-// document.querySelector('.header').appendChild(Icon);
+function render(e) {
+
+    // console.log (e.target.className);
+    console.log(e.target);
+
+    // if (e.target.className = 'menu-png' ) {
+        document.querySelector('.side-bar').style.width = '200px';
+    // }
+    // if (e.target.id = 'mainId' ) {
+    //     document.querySelector('.side-bar').style.width = '0px';
+    // }
+
+    // problem both these if statments firing make seperate event functions?
+    // whats more practical? Also need to add e listener for if clicked on header/footer
+    // or make something if clicked != sidebar more efficient
+    
+
+}
+
+function renderOff(e) {
+    if (e.target.id != '.side-bar' ) {
+        document.querySelector('.side-bar').style.width = '0px';
+    }
+}
+
 
 
 
