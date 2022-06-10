28ba4fda009bf0d19e1c408fabd16c4815161872
SteezyLeezy <ericle201727@gmail.com>
2022-06-09 20:01:46 -0700

    added submitTodo function that will add the form info to an array that will be displaying the todos

--- src/DOM.js
+++ src/DOM.js
@@ -5,10 +5,12 @@ const DOM = (() => {
     const form = document.querySelector('.form-container');
     const menuBtn = document.querySelector('.menu-btn');
     const nonMenu = document.querySelector('.main');
+    const submitBtn = document.querySelector('.submit-btn');
     // Event Listeners
     todo.addEventListener('click', todoFormDis);
     menuBtn.addEventListener('click',sideBarDis);
     nonMenu.addEventListener('click',sideBarOff);
+    submitBtn.addEventListener('click',submitTodo)
 
     function todoFormDis(e) {
         if (form.style.display == 'block') {
@@ -27,6 +29,11 @@ const DOM = (() => {
     function sideBarOff(e) {
         document.querySelector('.side-bar').style.width = '0px';
         document.querySelector('.main').style.marginLeft = '0px';
+    }
+
+    function submitTodo(e) {
+        const title = document.querySelector('#title-txt').value;
+        alert(title);
     }
 
     return {
