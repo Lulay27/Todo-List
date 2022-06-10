ff424b5ae1a42e90dc9466b0c873eba3ce8f6780
SteezyLeezy <ericle201727@gmail.com>
2022-06-09 19:47:03 -0700

    cleaned up js files moved event functions and eventlisteners into DOM.js

--- src/DOM.js
+++ src/DOM.js
@@ -1,12 +1,38 @@
 const DOM = (() => {
 
+    // DOM constants
     const todo = document.querySelector('.add-todo-btn');
-    todo.addEventListener('click', display);
+    const form = document.querySelector('.form-container');
+    const menuBtn = document.querySelector('.menu-btn');
+    const nonMenu = document.querySelector('.main');
+    // Event Listeners
+    todo.addEventListener('click', todoFormDis);
+    menuBtn.addEventListener('click',sideBarDis);
+    nonMenu.addEventListener('click',sideBarOff);
 
-    function display(e) {
-    document.querySelector('.todo-form').style.display = 'block';
+    function todoFormDis(e) {
+        if (form.style.display == 'block') {
+            form.style.display = 'none';
+        }
+        else if (form.style.display == 'none') {
+            form.style.display = 'block';
+        }
+    }
 
-}
+    function sideBarDis(e) {
+        document.querySelector('.side-bar').style.width = '200px';
+        document.querySelector('.main').style.marginLeft = '200px';
+    }
+
+    function sideBarOff(e) {
+        document.querySelector('.side-bar').style.width = '0px';
+        document.querySelector('.main').style.marginLeft = '0px';
+    }
+
+    return {
+
+    }
+
 })();
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import _, { difference } from 'lodash';
 import './styles/style.css';
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
@@ -8,36 +7,6 @@ import '@fortawesome/fontawesome-free/js/brands'
 import DOM from './DOM.js';
 
 
-
-// library.add(faMeteor);
-// dom.watch();
-
-// import Icon from './components/menu.png';
-
-const menuBtn = document.querySelector('.menu-btn');
-const nonMenu = document.querySelector('.main');
-// const notSide = document.querySelectorAll('body :not(.side-bar');
-const sideBar = document.querySelector('.side-bar');
-
-menuBtn.addEventListener('click',render);
-nonMenu.addEventListener('click',renderOff);
-
-
-function render(e) {
-    // if (e.target.className = 'fa fa-bars') {
-        document.querySelector('.side-bar').style.width = '200px';
-        document.querySelector('.main').style.marginLeft = '200px';
-    // }
-}
-
-function renderOff(e) {
-    // if (!sideBar.contains(e.target)) {
-        document.querySelector('.side-bar').style.width = '0px';
-        document.querySelector('.main').style.marginLeft = '0px';
-    // }
-}
-
-const main = document.querySelector('.main');
 
 
 
