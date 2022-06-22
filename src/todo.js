f48aa1890898df33cbaf2fdff8338007e50a0c29
SteezyLeezy <ericle201727@gmail.com>
2022-06-22 01:44:11 -0700

    completely changed how I am going to display todos within array. First since each todoUI is going to have many pieces of data in each div (delete todo btn, edit todo btn, check box..etc) I think it would be cleaner and easier to understand by directly writing the html inside ` quotes inside todo.js render...also working on creating a function to clean it

--- src/todo.js
+++ src/todo.js
@@ -2,13 +2,35 @@ import DOM from './DOM.js'
 
 // iterate through array containing todos, located in DOM.js
 //testing create div with innerhtml containing array titles i..
-let titlediv;
+let todoUI;
 function render() {
     for (let i = 0; i < DOM.myArr.length; i++) {
-        titlediv = document.createElement('li');
-        titlediv.innerHTML = DOM.myArr[i].title;
+        // titlediv = document.createElement('li');
+        // titlediv.id = "object" + i;
+        // const checkBox = document.createElement('input');
+
+        // titlediv.innerHTML = DOM.myArr[i].title;
+        
+        // checkBox.type = 'checkbox';
+        // titlediv.appendChild(checkBox);
+        // getting complicated should I just create the html inside a single innerHTML line?
+        //test
+
+        todoUI = document.createElement("div");
+        todoUI.id = "object" + i;
+        todoUI.innerHTML += `
+        <div class='todo-data'>
+            <input type="checkbox">
+            ${DOM.myArr[i].title}
+        </div>
+        `
+
+
     }
-    document.querySelector('.todo-container').appendChild(titlediv);
+    // const checkBox = document.createElement('input');
+    // checkBox.type = 'checkbox';
+    
+    document.querySelector('.todo-container').appendChild(todoUI);
 }
 // todo constructor factory
 // const todo = (title,note,due,prio) => {
