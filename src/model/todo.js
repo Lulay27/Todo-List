5960648eb02501953f4322e139492e45a1593ba3
SteezyLeezy <ericle201727@gmail.com>
2022-08-03 13:40:17 -0700

    figured out problem, it was todoArr containing objects with _title etc as parameters caused from setters in Todo class. Now localstorage api is working fully

--- src/model/todo.js
+++ src/model/todo.js
@@ -7,54 +7,54 @@ export class Todo {
         this.prio = prio;
         this.id = id;
     }
-    get title() {
-        return this._title;
-    }
+    // get title() {
+    //     return this._title;
+    // }
 
-    set title(newTitle) {
-        if (newTitle) {
-            return this._title = newTitle;
-        }
-    }
+    // set title(newTitle) {
+    //     if (newTitle) {
+    //         return this._title = newTitle;
+    //     }
+    // }
 
-    get prio() {
-        return this._prio;
-    }
+    // get prio() {
+    //     return this._prio;
+    // }
 
-    set prio(newPrio) {
-        if (newPrio) {
-            this._prio = newPrio;
-        }
-    }
+    // set prio(newPrio) {
+    //     if (newPrio) {
+    //         this._prio = newPrio;
+    //     }
+    // }
     
-    get due() {
-        return this._due;
-    }
+    // get due() {
+    //     return this._due;
+    // }
 
-    set due(newDue) {
-        if (newDue) {
-            this._due = newDue;    
-        }
-    }
+    // set due(newDue) {
+    //     if (newDue) {
+    //         this._due = newDue;    
+    //     }
+    // }
 
-    get note() {
-        return this._note;
-    }
+    // get note() {
+    //     return this._note;
+    // }
 
-    set note(newNote) {
-        if (newNote) {
-            this._note = newNote;    
-        }
-    }
+    // set note(newNote) {
+    //     if (newNote) {
+    //         this._note = newNote;    
+    //     }
+    // }
 
-    get id() {
-        return this._id;
-    }
+    // get id() {
+    //     return this._id;
+    // }
 
-    set id(newId) {
-        if (newId) {
-            this._id = newId;
-        }
-    }
+    // set id(newId) {
+    //     if (newId) {
+    //         this._id = newId;
+    //     }
+    // }
     // add get/set for title later when adding function for that in view
 }
